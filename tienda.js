8af2d9be2da3e000d385a911eca73b3e2f065802
Cristian Gomez <gomez18cristian@gmail.com>
2021-05-26 20:47:39 -0500

    funcionando con 1 h

--- tienda.js
+++ tienda.js
@@ -4,12 +4,15 @@ let marco = document.getElementById("superMercado");
 let mostrarClientes = document.getElementById("mostrarClientes");
 let mostrarClientes2 = document.getElementById("mostrarClientes2");
 let mostrarClientes3 = document.getElementById("mostrarClientes3");
-let horas = document.getElementById("horas");
 let divCaja1 = document.getElementById("caja1");
 let divCaja2 = document.getElementById("caja2");
 let divCaja3 = document.getElementById("caja3");
 let divCaja4 = document.getElementById("caja4");
 let subtitulo = document.getElementById("subtitulo");
+let resultadoR = document.getElementById("resultadoR");
+let resultadoG = document.getElementById("resultadoG");
+let colaG = document.getElementById("colaG");
+let colaR = document.getElementById("colaR");
 
 // variables temporales
 
@@ -18,212 +21,256 @@ let caja2 = true;
 let caja3 = true;
 let caja4 = true;
 
+const horas2 = prompt("Numero de horas");
+const numeroHoras = Number(horas2);
+console.log(numeroHoras)
 
-// vector de tiempo de llegada
-const numeroHoras = Number(horas.value);    
-const numeroClientes = Math.floor(Math.random() * (110 - 70)) + 70; // media 90 clientes por hora
-const clientesTotales = numeroHoras * numeroClientes;
-let vectorLlegada = []
-let vectorProductos = []
-const mediaLlegada = 5;
-const tiempoProducto = 50; // en este momento esta en 50    
-const min = 1;
-for (let i = 0; i < clientesTotales; i++) {
+
+// Tiempo:
+const tiempoProducto = 20 / numeroHoras; // en este momento esta en 50    
+const tiempoAnimacion = 200 / numeroHoras; // estaba en 500
+const tiempoEntrePersonas = 200 * numeroHoras; // estaba en 500
+
+
+const numeroClientesG = Math.floor(Math.random() * (110 - 80)) + 80; // media 95 clientes por hora
+const clientesTotalesG = numeroHoras * numeroClientesG;
+let vectorLlegada = [];
+let vectorProductos = [];
+const mediaLlegada = 6;
+const maxProductos = 100;
+const min = 11;
+for (let i = 0; i < clientesTotalesG; i++) {
     vectorLlegada[i] = Math.floor(Math.random() * mediaLlegada);
-    vectorProductos[i] = Math.floor(Math.random() * (clientesTotales - min)) + min;
+    vectorProductos[i] = Math.floor(Math.random() * (maxProductos - min)) + min;
 }
 
-subtitulo.innerHTML = vectorProductos;
+// cola rapida
+
+// const restaR = 14; // rango 23 - 14
+const restaR = Math.floor(Math.random() * (23 - 14)) + 14
+const numeroClientesR = numeroClientesG - restaR;
+const clientesTotalesR = numeroHoras * numeroClientesR;
+let vectorLlegadaR = []
+let vectorProductosR = []
+const mediaLlegadaR = 7;
+for (let i = 0; i < clientesTotalesR; i++) {
+    vectorLlegadaR[i] = Math.floor(Math.random() * mediaLlegadaR);
+    vectorProductosR[i] = Math.floor(Math.random() * (11 - 1)) + 1;
+}
+
+
+// ESTO esta de mientras
+// subtitulo.innerHTML = vectorLlegadaR
+console.log(vectorProductos)
+console.log(vectorLlegada)
+console.log(vectorProductosR)
+console.log(vectorLlegadaR)
+console.log("tiempo: ",tiempoProducto, "  -  ", tiempoAnimacion, "  -  " , tiempoEntrePersonas)
 
 
 const presionarBoton = () => {
-    console.log(vectorProductos)
-    console.log(vectorLlegada)   
-    llegadaClientes();
-    llegadaClientes2();
     
+    llegadaClientes();
+    llegadaClientesR();
+    colaGeneral();
+    colaRapida()
+
 };
 
-const resolveAfterxSeconds = (x,i) => {
+const resolveAfterxSeconds = (x, i) => {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve(vectorProductos[i]);
-        }, x * 500);
+        }, x * tiempoEntrePersonas);
     });
 }
+
+const resolveRapida = (x, i) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(vectorProductosR[i]);
+        }, x * tiempoEntrePersonas);
+    });
+}
+
+
 
 const promesa2 = () => {
     return new Promise(resolve => {
-        setTimeout(() => {            
+        setTimeout(() => {
             resolve();
-        },100);
+        }, 100);
     });
 }
 
-const llegadaClientes2 = async() => {
-    let cont = 0;    
-    for(const element of vectorLlegada) {
-        let numProduct = await resolveAfterxSeconds(element, cont);
-        if (numProduct > 10) {
-            mostrarClientes2.innerHTML += `<div class="clientesCola">${numProduct}</div>`;    
-        } else {
-            mostrarClientes3.innerHTML += `<div class="clientesCola">${numProduct}</div>`;    
-        }
+var sumaProductosR = 0;
+
+const colaGeneral = async () => {
+    let cont = 0;
+    for (const element of vectorLlegada) {
+        let numProduct = await resolveAfterxSeconds(element, cont);        
+        mostrarClientes2.innerHTML += `<div class="clientesCola">${numProduct}</div>`;
+        sumaProductosR += numProduct;        
         cont++;
     }
-}     
+}
+
+const colaRapida = async () => {
+    let cont = 0;
+    for (const element of vectorLlegadaR) {
+        let numProduct = await resolveRapida(element, cont);
+        mostrarClientes3.innerHTML += `<div class="clientesCola">${numProduct}</div>`;            
+        cont++;
+    }
+}
 
 
 
 
 
-const llegadaClientes = async() => {
-    let cont = 0;    
-    for(const element of vectorLlegada) {
+
+const llegadaClientes = async () => {
+    let cont = 0;
+    for (const element of vectorLlegada) {
         let numProduct = await resolveAfterxSeconds(element, cont);
-        
-        let seguir = true;    
 
-        // ELIMINAR
-        // let backupMostrarCliente = mostrarClientes.innerHTML;        
-        // mostrarClientes.innerHTML += `<div class="clientesCola">${numProduct}</div>`        
-        
+        let seguir = true;
 
-        while (seguir == true) {            
+        while (seguir == true) {
 
-            if (numProduct > 10) {
+            // CAJA GENERAL
 
-                // CAJA GENERAL
-    
-                if (caja1) {
+            if (caja1) {
 
-                    seguir = false;
-                    caja1 = false; 
-                    // console.log("contador" , cont)
-                    // console.log("padre", mostrarClientes.innerHTML);
-                    // // console.log("hijo i", mostrarClientes.children[0]);
-                    // console.log("hijo nodes", mostrarClientes.childNodes[0]);
-                    
-                    // // mostrarClientes.removeChild(cont);
-                    // // mostrarClientes.firstChild.remove();
-                    // console.log("padre2", mostrarClientes.innerHTML)
-                    
-                    setTimeout(() =>{
-                        // mostrarClientes.innerHTML = backupMostrarCliente; 
-                        // mostrarClientes.innerHTML = ''; 
-                        mostrarClientes2.childNodes[0].remove();
-                        divCaja1.innerHTML = `<div class="cliente">${numProduct}</div>`;                    
-                    }, 250)
-                    
-                    // esperar el tiempo del mercado
-                    let timeCaja = numProduct * tiempoProducto
-                    console.log("timeCaja",timeCaja)                  
-    
-                    setTimeout(()=>{                        
-                    }, timeCaja + 250)
-    
-                    setTimeout(()=>{
-                        divCaja1.innerHTML = '';
-                        caja1 = true;
-                    }, timeCaja + 500)
+                seguir = false;
+                caja1 = false;
 
-                    
-                    
-                }else if (caja2){
-                    seguir = false;
-                    caja2 = false;      
-                    setTimeout(() =>{
-                        // mostrarClientes.innerHTML = backupMostrarCliente; 
-                        // mostrarClientes.innerHTML = ''; 
-                        mostrarClientes2.childNodes[0].remove();
-                        divCaja2.innerHTML = `<div class="cliente">${numProduct}</div>`;                    
-                    }, 250)
-                    
-                    // esperar el tiempo del mercado
-                    let timeCaja = numProduct * tiempoProducto
-                    console.log("timeCaja",timeCaja)                  
-    
-                    setTimeout(()=>{                        
-                    }, timeCaja + 250)
-    
-                    setTimeout(()=>{
-                        divCaja2.innerHTML = '';
-                        caja2 = true;
-                    }, timeCaja + 500)
-                    
-    
-                }else if (caja3){
 
-                    seguir = false;
-                    caja3 = false;      
-                    setTimeout(() =>{
-                        // mostrarClientes.innerHTML = backupMostrarCliente; 
-                        // mostrarClientes.innerHTML = ''; 
-                        mostrarClientes2.childNodes[0].remove();
-                        divCaja3.innerHTML = `<div class="cliente">${numProduct}</div>`;                    
-                    }, 250)
-                    
-                    // esperar el tiempo del mercado
-                    let timeCaja = numProduct * tiempoProducto
-                    console.log("timeCaja",timeCaja)                  
-    
-                    setTimeout(()=>{                        
-                    }, timeCaja + 250)
-    
-                    setTimeout(()=>{
-                        divCaja3.innerHTML = '';
-                        caja3 = true;
-                    }, timeCaja + 500)
-                    
-                }else{
-                    await promesa2();
-                    seguir = true;    
-                }   
-    
-                
-            }else{
-    
-                // CAJA RAPIDA
-    
-                if (caja4) {
+                setTimeout(() => {
+                    mostrarClientes2.childNodes[0].remove();
+                    divCaja1.innerHTML = `<div class="cliente">${numProduct}</div>`;
+                }, tiempoAnimacion)
 
-                    seguir = false;
-                    caja4 = false;      
-                    setTimeout(() =>{
-                        // mostrarClientes.innerHTML = backupMostrarCliente; 
-                        // mostrarClientes.innerHTML = ''; 
-                        mostrarClientes2.childNodes[0].remove();
-                        divCaja4.innerHTML = `<div class="cliente">${numProduct}</div>`;                    
-                    }, 250)
-                    
-                    // esperar el tiempo del mercado
-                    let timeCaja = numProduct * tiempoProducto
-                    console.log("timeCaja",timeCaja)                  
-    
-                    setTimeout(()=>{
-                        console.log(numProduct)
-                    }, timeCaja + 250)
-    
-                    setTimeout(()=>{
-                        divCaja4.innerHTML = '';
-                        caja4 = true;
-                    }, timeCaja + 500)
-                    
-                    
-                }else{
-                    await promesa2();
-                    seguir = true;                    
-                }
-            
-            }   
+                // esperar el tiempo del mercado
+                let timeCaja = numProduct * tiempoProducto                
+
+                setTimeout(() => {
+                }, timeCaja + tiempoAnimacion)
+
+                setTimeout(() => {
+                    divCaja1.innerHTML = '';
+                    caja1 = true;
+                }, timeCaja + (tiempoAnimacion/2))
+
+
+
+            } else if (caja2) {
+                seguir = false;
+                caja2 = false;
+                setTimeout(() => {
+                    mostrarClientes2.childNodes[0].remove();
+                    divCaja2.innerHTML = `<div class="cliente">${numProduct}</div>`;
+                }, tiempoAnimacion)
+
+                // esperar el tiempo del mercado
+                let timeCaja = numProduct * tiempoProducto                
+
+                setTimeout(() => {
+                }, timeCaja + tiempoAnimacion)
+
+                setTimeout(() => {
+                    divCaja2.innerHTML = '';
+                    caja2 = true;
+                }, timeCaja + (tiempoAnimacion/2))
+
+
+            } else if (caja3) {
+
+                seguir = false;
+                caja3 = false;
+                setTimeout(() => {
+                    mostrarClientes2.childNodes[0].remove();
+                    divCaja3.innerHTML = `<div class="cliente">${numProduct}</div>`;
+                }, tiempoAnimacion)
+
+                // esperar el tiempo del mercado
+                let timeCaja = numProduct * tiempoProducto                
+
+                setTimeout(() => {
+                }, timeCaja + tiempoAnimacion)
+
+                setTimeout(() => {
+                    divCaja3.innerHTML = '';
+                    caja3 = true;
+                }, timeCaja + (tiempoAnimacion/2))
+
+            } else {
+                await promesa2();
+                seguir = true;
+            }
+
+
+        }
+        cont++;
+        if(cont == numeroClientesG){
+            resultado();
+        }
+    };
+}
+
+
+const llegadaClientesR = async () => {
+    let cont = 0;
+    for (const element of vectorLlegadaR) {
+        let numProduct = await resolveRapida(element, cont);
+
+        let seguir = true;
+
+        while (seguir == true) {
+
+            // CAJA RAPIDA
+
+            if (caja4) {
+
+                seguir = false;
+                caja4 = false;
+                setTimeout(() => {
+                    mostrarClientes3.childNodes[0].remove();
+                    divCaja4.innerHTML = `<div class="cliente">${numProduct}</div>`;
+                }, tiempoAnimacion)
+
+                // esperar el tiempo del mercado
+                let timeCaja = numProduct * tiempoProducto                
+
+                setTimeout(() => {
+                }, timeCaja + tiempoAnimacion)
+
+                setTimeout(() => {
+                    divCaja4.innerHTML = '';
+                    caja4 = true;
+                }, timeCaja + (tiempoAnimacion/2))
+
+
+            } else {
+                await promesa2();
+                seguir = true;
+            }
+
         }
 
-    
 
         cont++;
 
     };
 }
+
+const resultado = ()=>{
+    resultadoR.innerHTML += numeroClientesR;
+    resultadoG.innerHTML += numeroClientesG;
+    colaG.innerHTML += (sumaProductosR / numeroClientesR);
+    colaR.innerHTML += sumaProductos;
+}
+
 
 
 botonIniciar.onclick = () => {
